070fe4c9980af27f0a30bb99877d43ceb0d59a8e
RedSTAR.Cheng <74100786+RedSTARO@users.noreply.github.com>
2022-04-08 09:33:30 +0800

    Update hitokoto.py

--- classReminder/hitokoto.py
+++ classReminder/hitokoto.py
@@ -3,6 +3,8 @@ import ast
 from debug import *
 
 url = "https://v1.hitokoto.cn?c=d&c=k&c=i"
+# TODO: Use "https://v1.hitokoto.cn?c=d&c=k&c=i&encode=json" to get json form; or "https://v1.hitokoto.cn?c=d&c=k&c=i&encode=text" to get pure text.
+# Details at "https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%8F%82%E6%95%B0"
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
 
 def hitokoto():
