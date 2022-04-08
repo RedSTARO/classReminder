6b30e6aa2e60e123d2b07aae80a49e5ffd28e257
RedSTAR.Cheng <74100786+RedSTARO@users.noreply.github.com>
2022-04-08 09:53:59 +0800

    Update debug.py

--- classReminder/debug.py
+++ classReminder/debug.py
@@ -8,31 +8,41 @@ proDir = os.path.split(os.path.realpath(__file__))[0]
 configPath = os.path.join(proDir, "config.ini")
 conf = configparser.ConfigParser()
 conf.read(configPath)
+logLevel = conf.get("log", "level")
 
 timeZone = pytz.timezone("Asia/Shanghai")
 path = conf.get("path", "log")
 
 def log(str_, level = "info"):
-    print(str_)
-    whichFun = str(sys._getframe().f_back.f_code.co_filename).split("/")[-1].split("\\")[-1]
-    if level = "info":
-    	with open(path + "logs_info.log", "a") as logFile:
-        	logFile.write(f"[{str(datetime.datetime.now(timeZone).replace(tzinfo=None))}]" + 
-                      	f"[{whichFun}] " + 
-                      	str(str_) + 
-						  "\n")
-	elif level = "error":
-		with open(path + "logs_error.log", "a") as logFile:
-        	logFile.write(f"[{str(datetime.datetime.now(timeZone).replace(tzinfo=None))}]" + 
-                      	f"[{whichFun}] " + 
-                      	str(str_) + 
-						  "\n")
-	elif level = "warning":
-		with open(path + "logs_warning.log", "a") as logFile:
-        	logFile.write(f"[{str(datetime.datetime.now(timeZone).replace(tzinfo=None))}]" + 
-                      	f"[{whichFun}] " + 
-                      	str(str_) + 
-						  "\n")
+#     print(str_)
+	if logLevel = "info":
+    	whichFun = str(sys._getframe().f_back.f_code.co_filename).split("/")[-1].split("\\")[-1]
+    	if level = "info":
+    		with open(path + "logs_info.log", "a") as logFile:
+        		logFile.write(f"[{str(datetime.datetime.now(timeZone).replace(tzinfo=None))}]" + 
+                      		f"[{whichFun}] " + 
+                      		str(str_) + 
+						  	"\n")
+	if logLevel = "dev":
+		whichFun = str(sys._getframe().f_back.f_code.co_filename).split("/")[-1].split("\\")[-1]
+    	if level = "info":
+    		with open(path + "logs_info.log", "a") as logFile:
+        		logFile.write(f"[{str(datetime.datetime.now(timeZone).replace(tzinfo=None))}]" + 
+                      		f"[{whichFun}] " + 
+                      		str(str_) + 
+						  	"\n")
+		elif level = "error":
+			with open(path + "logs_error.log", "a") as logFile:
+        		logFile.write(f"[{str(datetime.datetime.now(timeZone).replace(tzinfo=None))}]" + 
+                      		f"[{whichFun}] " + 
+                      		str(str_) + 
+						  	"\n")
+		elif level = "warning":
+			with open(path + "logs_warning.log", "a") as logFile:
+        		logFile.write(f"[{str(datetime.datetime.now(timeZone).replace(tzinfo=None))}]" + 
+                      		f"[{whichFun}] " + 
+                      		str(str_) + 
+						  	"\n")
 
 log(f"Matched config.ini, in path {configPath}")
 
